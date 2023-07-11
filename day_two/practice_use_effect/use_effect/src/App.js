5ca3ab8dae8d86f39b50fc43bb7c569c2f688ffb
Linda Vadysirisack <linda.vady@gmail.com>
2023-07-11 14:06:30 -0500

    updating wk 10

---
+++ day_two/practice_use_effect/use_effect/src/App.js
@@ -0,0 +1,13 @@
+import Advanced from "./Advanced";
+import People from "./People";
+
+function App() {
+  return (
+    <div className="App">
+      <People />
+      <Advanced />
+    </div>
+  );
+}
+
+export default App;
